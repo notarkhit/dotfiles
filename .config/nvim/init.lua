e3689ec5cdfd46aed4de37981d892b7849a6ea81
notarkhit <notarkhit@gmail.com>
2025-01-21 13:50:54 +0530

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -680,6 +680,7 @@ require("lazy").setup({
 				"lua-language-server",
 				"clangd",
 				"bashls",
+				"hyprls",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -881,26 +882,26 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		-- NOTE: tokyonight:
-		-- "folke/tokyonight.nvim",
-		-- priority = 1000, -- Make sure to load this before all the other start plugins.
-		-- init = function()
-		-- 	-- Load the colorscheme here.
-		-- 	-- Like many other themes, this one has different styles, and you could load
-		-- 	-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		-- 	vim.cmd.colorscheme("tokyonight-night")
-		--
-		-- 	-- You can configure highlights by doing something like:
-		-- 	vim.cmd.hi("Comment gui=none")
-		-- end,
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-night")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
 		--NOTE: tokyonight end
 		-- NOTE: nord theme
-		"gbprod/nord.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nord").setup({})
-			vim.cmd.colorscheme("nord")
-		end,
+		-- "gbprod/nord.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	require("nord").setup({})
+		-- 	vim.cmd.colorscheme("nord")
+		-- end,
 		-- NOTE: nord theme end
 	},
 
