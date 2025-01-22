00a25bd36e571e2ac0c62e77b6248fb6b62dffdf
notarkhit <notarkhit@gmail.com>
2025-01-22 13:56:52 +0530

    add oil.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -329,6 +329,16 @@ require("lazy").setup({
 		},
 	},
 
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil' @type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer
+		-- nvim-web-devicons
+	},
+
 	-- NOTE: Plugins can specify dependencies.
 	--
 	-- The dependencies are proper plugin specifications as well - anything
