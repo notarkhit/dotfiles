d10a2b7ae4f0a6ea49b5ae70ae90333ce63c03d5
notarkhit <notarkhit@gmail.com>
2025-01-25 10:22:57 +0530

    add render markdown plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -342,6 +342,16 @@ require("lazy").setup({
 		-- nvim-web-devicons
 	},
 
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+
 	"ThePrimeagen/vim-be-good",
 
 	-- NOTE: Plugins can specify dependencies.
