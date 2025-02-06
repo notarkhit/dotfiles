fccac1f678c2e36cd13e49963f477b73c57cc843
notarkhit <notarkhit@gmail.com>
2025-02-06 08:31:32 +0530

    add powermenu script

---
+++ .config/scripts/power-menu.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+config="$HOME/.config/rofi/config.rasi"
+
+actions=$(echo -e "   Lock\n   Shutdown\n   Reboot\n $(printf '\u200A')  Suspend\n   Hibernate\n   Logout")
+
+# Display logout menu
+
+if pgrep -x "rofi" > /dev/null; then
+    pkill -x "rofi"
+else
+	selected_option=$(echo -e "$actions" | rofi -dmenu -i -config "${config}" -p "")
+fi
+
+# Perform actions based on the selected option
+case "$selected_option" in
+*Lock)
+  hyprlock
+  ;;
+*Shutdown)
+  systemctl poweroff
+  ;;
+*Reboot)
+  systemctl reboot
+  ;;
+*Suspend)
+  systemctl suspend
+  ;;
+*Hibernate)
+  systemctl hibernate
+  ;;
+*Logout)
+  hyprctl dispatch exit 0
+  ;;
+esac
