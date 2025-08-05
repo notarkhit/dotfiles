21f78847b1aa85c053b4701d7dd08a780408c4ed
notarkhit <notarkhit@gmail.com>
2025-08-06 00:34:10 +0530

    Add volume script

---
+++ .scripts/volume.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to get the current volume
+get_current_volume() {
+    pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//'
+}
+
+# Check command line arguments
+if [[ "$#" != 1 || ! ("$1" == "inc" || "$1" == "dec" || "$1" == "mute" ) ]]; then
+    printf "Usage: $0 [inc|dec|mute]\n"
+    exit 1
+fi
+
+# Check if pactl is installed
+if ! command -v pactl &> /dev/null; then
+    echo "Error: pactl is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Perform volume adjustment
+if [[ "$1" == "inc" ]]; then
+    [ "$(get_current_volume)" -lt 150 ] && pactl set-sink-volume @DEFAULT_SINK@ +2%
+elif [[ "$1" == "dec" ]]; then
+    pactl set-sink-volume @DEFAULT_SINK@ -2%
+elif [[ "$1" == "mute" ]]; then
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+fi
+
+VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
+dunstify -r 9993 -t 3000 -a "  Volume" -h int:value:"$VOLUME" "Volume: ${VOLUME}%"
