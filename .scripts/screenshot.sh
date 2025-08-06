221cc27b97ffa7a7f9bb47297a7b0b18d38fecc4
notarkhit <notarkhit@gmail.com>
2025-08-06 08:17:25 +0530

    Update screenshot script

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -9,11 +9,15 @@ done
 # Capture screenshot
 screenshot="$(slurp -b 00000044 -c 333333ff )"
 
+#Temp file to save the screenshot
+tempimg=$(mktemp --suffix=.png)
+
 # Process the screenshot and copy to clipboard
-grim -g "$screenshot" - | magick - -shave 2x2 PNG:- | wl-copy
+grim -g "$screenshot" - | magick - -shave 2x2 PNG:"$tempimg"
+wl-copy < "$tempimg"
 
 # Notify screenshot has copied to clipboard
-dunstify -t 3000 -u low -a screenshot "Screenshot copied to clipboard"
+dunstify -t 3000 -u low -a "Screenshot" -i "$tempimg" "Screenshot copied to clipboard"
 
 # Paste to clipboard and use swappy for further processing
 wl-paste | swappy -f -
