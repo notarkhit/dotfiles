b2ef27f6a26eb63434cec2808192df0fdffe1b78
notarkhit <notarkhit@gmail.com>
2025-08-09 11:51:53 +0530

    Update volume script

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -5,6 +5,10 @@ get_current_volume() {
     pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//'
 }
 
+get_muted() {
+	pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}'
+}
+
 # Check command line arguments
 if [[ "$#" != 1 || ! ("$1" == "inc" || "$1" == "dec" || "$1" == "mute" ) ]]; then
     printf "Usage: $0 [inc|dec|mute]\n"
@@ -20,11 +24,21 @@ fi
 # Perform volume adjustment
 if [[ "$1" == "inc" ]]; then
     [ "$(get_current_volume)" -lt 150 ] && pactl set-sink-volume @DEFAULT_SINK@ +1%
+	volumeicon=/usr/share/icons/Papirus/32x32/symbolic/status/audio-volume-high-symbolic.svg
 elif [[ "$1" == "dec" ]]; then
     pactl set-sink-volume @DEFAULT_SINK@ -1%
+	volumeicon=/usr/share/icons/Papirus/32x32/symbolic/status/audio-volume-medium-symbolic.svg
+	if [[ "$(get_current_volume)" -eq 0 ]]; then
+		volumeicon=/usr/share/icons/Papirus/32x32/symbolic/status/audio-volume-low-symbolic.svg
+	fi
 elif [[ "$1" == "mute" ]]; then
     pactl set-sink-mute @DEFAULT_SINK@ toggle
+	if [ "$(get_muted)" = "yes" ]; then
+		volumeicon=/usr/share/icons/Papirus/32x32/symbolic/status/audio-volume-muted-symbolic.svg
+	else 
+		volumeicon=/usr/share/icons/Papirus/32x32/symbolic/status/audio-volume-medium-symbolic.svg
+	fi
 fi
 
 VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed 's/%//')
-dunstify -r 9993 -t 3000 -a "  Volume" -h int:value:"$VOLUME" "Volume: ${VOLUME}%" -i "/usr/share/icons/Papirus/32x32/devices/audio-headset.svg"
+dunstify -r 9993 -t 3000 -a "Volume" -h int:value:"$VOLUME" "Volume: ${VOLUME}%" -i $volumeicon
