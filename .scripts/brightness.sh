4fc433bb848562b2bc23b7fdc1510b37ff4cba72
notarkhit <notarkhit@gmail.com>
2025-08-13 07:59:29 +0530

    update brightness.sh

--- .scripts/brightness.sh
+++ .scripts/brightness.sh
@@ -20,13 +20,15 @@ if ! command -v brightnessctl &> /dev/null; then
   echo "Error: brightnessctl is not installed. Please install it." >&2
   exit 1
 fi
-
+BRIGHTNESS_PERCENT=$(brightnessctl | awk -F '[()]' '/Current brightness/ {print $2}' | tr -d '%')
 # Perform brightness adjustment
 if [[ "$1" == "inc" ]]; then
   brightnessctl set +1% > /dev/null
   notify_brightness
 elif [[ "$1" == "dec" ]]; then
+	if ((BRIGHTNESS_PERCENT > 1 )); then
   brightnessctl set 1%- > /dev/null
+	fi
   notify_brightness
 fi
 
