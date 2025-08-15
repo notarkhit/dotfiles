9b7bd937019207569eab19e329d78418afde1d3a
notarkhit <notarkhit@gmail.com>
2025-08-15 22:13:33 +0530

    fix brightness script

--- .scripts/brightness.sh
+++ .scripts/brightness.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 function notify_brightness() {
-  # Function to show brightness notification
-  MAX_BRIGHTNESS=$(brightnessctl max)
-  CURRENT_BRIGHTNESS=$(brightnessctl get) 
-
-  BRIGHTNESS_PERCENT=$(brightnessctl | awk -F '[()]' '/Current brightness/ {print $2}')
-  dunstify -r 9993 -t 3000 -a "  Brightness" -h int:value:"$BRIGHTNESS_PERCENT" "brightness: ${BRIGHTNESS_PERCENT}" -i "/usr/share/icons/Papirus/16x16/apps/brightness-systray.svg"
+	local BRIGHTNESS_PERCENT="$1"
+	if [[ -z "$BRIGHTNESS_PERCENT" ]]; then
+		BRIGHTNESS_PERCENT=$(brightnessctl | awk -F '[()]' '/Current brightness/ {print $2}')
+	fi
+	dunstify -r 9993 -t 3000 -a "  Brightness" -h int:value:"$BRIGHTNESS_PERCENT" "brightness: ${BRIGHTNESS_PERCENT}" -i "/usr/share/icons/Papirus/16x16/apps/brightness-systray.svg"
 }
 
 # Check command line arguments
@@ -23,12 +22,15 @@ fi
 BRIGHTNESS_PERCENT=$(brightnessctl | awk -F '[()]' '/Current brightness/ {print $2}' | tr -d '%')
 # Perform brightness adjustment
 if [[ "$1" == "inc" ]]; then
-  brightnessctl set +1% > /dev/null
-  notify_brightness
+	notify_brightness
+	brightnessctl set +1% > /dev/null
 elif [[ "$1" == "dec" ]]; then
 	if ((BRIGHTNESS_PERCENT > 1 )); then
-  brightnessctl set 1%- > /dev/null
+		brightnessctl set 1%- > /dev/null
+	elif ((BRIGHTNESS_PERCENT == 1)); then
+		notify_brightness 0%
+		exit 0
 	fi
-  notify_brightness
+		notify_brightness
 fi
 
