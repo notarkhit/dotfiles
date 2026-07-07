8518b257275522f269691747002e3d45fb72f97b
notarkhit <notarkhit@gmail.com>
2026-07-07 10:12:52 +0530

    style(hypr): enable blur and refine compositor settings

--- .config/hypr/modules/ux.lua
+++ .config/hypr/modules/ux.lua
@@ -3,43 +3,43 @@
 -----------------------
 
 hl.config({
-    general = {
-        gaps_in  = 0,
-        gaps_out = 0,
-        border_size = 0,
-        col = {
-            active_border   = "rgba(00000000)",
-            -- active_border = "rgba(59595900)",
-            inactive_border = "rgba(59595900)",
-        },
-        resize_on_border = false,
-        allow_tearing    = false,
-        layout           = "dwindle",
-    },
+	general = {
+		gaps_in          = 0,
+		gaps_out         = 0,
+		border_size      = 0,
+		col              = {
+			active_border   = "rgba(00000000)",
+			-- active_border = "rgba(59595900)",
+			inactive_border = "rgba(59595900)",
+		},
+		resize_on_border = false,
+		allow_tearing    = false,
+		layout           = "dwindle",
+	},
 
-    cursor = {
-        inactive_timeout = 0.5,
-    },
+	cursor = {
+		inactive_timeout = 0.5,
+	},
 
-    decoration = {
-        rounding         = 0,
-        active_opacity   = 0.97,
-        inactive_opacity = 0.85,
-        shadow = {
-            enabled      = false,
-            range        = 4,
-            render_power = 3,
-            color        = "rgba(1a1a1aee)",
-        },
-        blur = {
-            enabled  = false,
-            size     = 2,
-            passes   = 3,
-            vibrancy = 0.1696,
-        },
-    },
+	decoration = {
+		rounding         = 0,
+		active_opacity   = 0.97,
+		inactive_opacity = 0.85,
+		shadow           = {
+			enabled      = false,
+			range        = 4,
+			render_power = 3,
+			color        = "rgba(1a1a1aee)",
+		},
+		blur             = {
+			enabled  = true,
+			size     = 1,
+			passes   = 1,
+			vibrancy = 0.1696,
+		},
+	},
 
-    animations = {
-        enabled = true,
-    },
+	animations = {
+		enabled = true,
+	},
 })
